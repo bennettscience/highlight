34b5898e0d113f6a4ab0baea35bd386723a2158f
TSCBennett <b.bennett@techsmith.com>
2014-01-28 15:11:03 -0500

    added stylesheet to be injected, added appropriate code to background, manifest, and contentscripts

--- contentscript.js
+++ contentscript.js
@@ -1,8 +1,19 @@
 chrome.runtime.onMessage.addListener( 
-    function(request, sender, sendResponse) { 
+    function(request, sender, sendResponse) {     
         if (request.method == "getSelection") 
             sendResponse({data: window.getSelection().toString()});
-        else
-            sendResponse({});
+        else sendResponse({});
+        }
+)
+
+chrome.runtime.onMessage.addListener(
+    function addClass() {
+        if (request.method == "getSelection") {
+            var newSpan = document.createElement('span');
+            newSpan.setAttribute('class','hiliteNode');
+            document.getElementById('text').appendChild(newSpan);
+            newSpan.innerHTML = selectedText;
+        }
+        else return
     }
 )
