400ff31e373e332266ce629ae50029e3d0a59ee6
TSCBennett <b.bennett@techsmith.com>
2014-01-29 13:56:39 -0500

    highlight between different element tags working

--- js/contentscript.js
+++ js/contentscript.js
@@ -6,7 +6,7 @@
         }  
 ) */
 
-function getText(tabs) {
+/* function getText(tabs) {
     var text = window.getSelection().toString();
     alert(text)
     var selection = window.getSelection().getRangeAt(0);
@@ -24,4 +24,41 @@ function getText(tabs) {
     selection.insertNode(span);
 }
 
-getText();
+getText(); */
+
+function makeEditableAndHighlight(colour) {
+    var range, sel = window.getSelection();
+    if (sel.rangeCount && sel.getRangeAt) {
+        range = sel.getRangeAt(0);
+    }
+    document.designMode = "on";
+    if (range) {
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+    // Use HiliteColor since some browsers apply BackColor to the whole block
+    if (!document.execCommand("HiliteColor", false, '#FFFF00')) {
+        document.execCommand("BackColor", false, '#FFFF00');
+    }
+    document.designMode = "off";
+}
+
+/* function highlight(colour) {
+    var range;
+    if (window.getSelection) {
+        // IE9 and non-IE
+        try {
+            if (!document.execCommand("BackColor", false, '#FFFF00')) {
+                makeEditableAndHighlight(colour);
+            }
+        } catch (ex) {
+            makeEditableAndHighlight(colour)
+        }
+    } else if (document.selection && document.selection.createRange) {
+        // IE <= 8 case
+        range = document.selection.createRange();
+        range.execCommand("BackColor", false, '#FFFF00');
+    }
+} */
+
+makeEditableAndHighlight();
