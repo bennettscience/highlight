344b5f47f9b108b6358317f8c075790fcace522f
TSCBennett <b.bennett@techsmith.com>
2014-01-29 14:07:51 -0500

    cleaned up commenting, added attribution for highlight function

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,30 +1,10 @@
-/* chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {     
-        if (request.method == "getSelection") 
-            sendResponse({data: window.getSelection().toString()});
-        else sendResponse({});
-        }  
-) */
-
-/* function getText(tabs) {
+function getText(tabs) {
     var text = window.getSelection().toString();
-    alert(text)
-    var selection = window.getSelection().getRangeAt(0);
-    var selectedText = selection.extractContents();
-    var span = document.createElement("span");
-    span.style.backgroundColor = "yellow";
-    span.appendChild(selectedText);
-    span.onclick = function (ev) {
-        this.parentNode.insertBefore(
-            document.createTextNode(this.innerHTML),
-            this
-        );
-        this.parentNode.removeChild(this);
-    }
-    selection.insertNode(span);
 }
+getText();
 
-getText(); */
+// This script is from Tim Downs as posted to Stack Overflow
+// http://stackoverflow.com/a/1623974/2278429
 
 function makeEditableAndHighlight(colour) {
     var range, sel = window.getSelection();
@@ -42,23 +22,4 @@ function makeEditableAndHighlight(colour) {
     }
     document.designMode = "off";
 }
-
-/* function highlight(colour) {
-    var range;
-    if (window.getSelection) {
-        // IE9 and non-IE
-        try {
-            if (!document.execCommand("BackColor", false, '#FFFF00')) {
-                makeEditableAndHighlight(colour);
-            }
-        } catch (ex) {
-            makeEditableAndHighlight(colour)
-        }
-    } else if (document.selection && document.selection.createRange) {
-        // IE <= 8 case
-        range = document.selection.createRange();
-        range.execCommand("BackColor", false, '#FFFF00');
-    }
-} */
-
 makeEditableAndHighlight();
