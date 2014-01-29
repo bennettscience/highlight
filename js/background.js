886d90bd4f1d7f6a5ba2b4979c38b44c126ae648
TSCBennett <b.bennett@techsmith.com>
2014-01-29 14:26:31 -0500

    added a context menu highlight option

--- js/background.js
+++ js/background.js
@@ -6,6 +6,19 @@
     });
 }); */
 
-chrome.browserAction.onClicked.addListener(function(tabs) {
+//When the argument length is two, it is coming from the
+//context menu. A single argument is from the browser action.
+//Shared from http://stackoverflow.com/q/6499471/2278429
+function runHighlight() {
+    var tab = arguments.length == 2 ? arguments[1] : arguments[0];
     chrome.tabs.executeScript(null, {file: 'js/contentscript.js'})
-})
+}
+
+chrome.browserAction.onClicked.addListener(runHighlight);
+
+chrome.contextMenus.create({
+    title: "Hilight this!",
+    contexts: ['selection'],
+    onclick: runHighlight
+});
+
